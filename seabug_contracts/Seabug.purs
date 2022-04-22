096ec5050525e9b422930fe3620d6ecc163ab7c4
Neil Rutledge <neilrutledgephoto@gmail.com>
2022-04-22 01:30:17 -0400

    Fix broken function in Seabug module

--- seabug_contracts/Seabug.purs
+++ seabug_contracts/Seabug.purs
@@ -1,30 +1,25 @@
 module Seabug
   ( module Seabug.CallContract
   , connectWallet
-  , getWalletLovelace
+  , getWalletBalance
   )
   where
 
 import Prelude
 
 import Control.Promise (Promise, fromAff)
-import Data.BigInt (BigInt)
-import Data.Maybe (Maybe(..))
+import Data.Maybe (Maybe)
 import Effect (Effect)
 import QueryM (callNami)
 import Seabug.CallContract
 import Serialization.Types (Value)
-import Types.Value (valueToCoin')
 import Wallet (Wallet(..), mkNamiWalletAff)
 
 connectWallet :: Effect (Promise Wallet)
 connectWallet = fromAff mkNamiWalletAff
 
-getWalletLovelace :: Effect (Promise BigInt)
-getWalletLovelace = fromAff $ do
+getWalletBalance :: Effect (Promise (Maybe Value))
+getWalletBalance = fromAff $ do
   (Nami nami) <- mkNamiWalletAff
-  balance <- callNami nami _.getBalance
-  case balance of 
-    Just bal -> pure $ valueToCoin' bal
-    Nothing -> pure Nothing
+  callNami nami _.getBalance
 
